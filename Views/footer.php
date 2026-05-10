e88ac497d8765059055db267e9d7ac326eb39c33
Ovi <email_roviatunhasanah525@gmail.com>
2026-05-10 20:40:17 +0700

    footer

---
+++ Views/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+    <div class="footer-container">
+        <div class="footer-col">
+            <h3>fistar<span class="highlight">_creative</span></h3>
+            <p>Bantu bikin desain jadi lebih kreatif! Logo, Banner, Poster, ID Card, Sablon, DLL. Cocok buat UMKM! 😍</p>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-tiktok"></i></a>
+                <a href="#"><i class="fab fa-behance"></i></a>
+            </div>
+        </div>
+        <div class="footer-col">
+            <h4>Layanan</h4>
+            <ul>
+                <li>Logo & Branding</li>
+                <li>Banner & Poster</li>
+                <li>ID Card</li>
+                <li>Sablon</li>
+                <li>Undangan</li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>Kontak</h4>
+            <p><i class="fab fa-whatsapp"></i> 0812-4952-4077</p>
+            <p><i class="fab fa-instagram"></i> @fistar_creative</p>
+            <p><i class="far fa-envelope"></i> fistarcreative@gmail.com</p>
+        </div>
+        <div class="footer-col">
+            <h4>Jam Operasional</h4>
+            <p>Sen - Sab: 08.00 - 21.00</p>
+            <p>Minggu: 10.00 - 17.00</p>
+            <p class="online">🟢 Online 24/7 untuk konsultasi</p>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>© 2026 fistar_creative | Desain untuk UMKM Indonesia</p>
+    </div>
+</footer>
+</body>
+</html>
