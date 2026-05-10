ea99b5dcc63610246b2f86017a0a77fa5347be44
Ovi <email_roviatunhasanah525@gmail.com>
2026-05-10 20:38:36 +0700

    menu

---
+++ Models/menu.php
@@ -0,0 +1,57 @@
+<?php
+
+class Menu {
+    
+    public function getMenu() {
+        return [
+            [
+                "nama" => "Logotype", 
+                "harga" => "7.000", 
+                "gambar" => "logotype.jpg",
+                "kategori" => "Logo"
+            ],
+            [
+                "nama" => "Logo Vector/Maskot", 
+                "harga" => "15.000", 
+                "gambar" => "logo-vector.jpg",
+                "kategori" => "Logo"
+            ],
+            [
+                "nama" => "Logo Kombinasi", 
+                "harga" => "10.000", 
+                "gambar" => "logo-kombinasi.jpg",
+                "kategori" => "Logo"
+            ],
+            [
+                "nama" => "Banner/Daftar Menu", 
+                "harga" => "10.000", 
+                "gambar" => "banner.jpg",
+                "kategori" => "Promosi"
+            ],
+            [
+                "nama" => "Poster", 
+                "harga" => "10.000", 
+                "gambar" => "poster.jpg",
+                "kategori" => "Promosi"
+            ],
+            [
+                "nama" => "ID Card (3 Item)", 
+                "harga" => "10.000", 
+                "gambar" => "idcard.jpg",
+                "kategori" => "Kartu"
+            ],
+            [
+                "nama" => "Sablon", 
+                "harga" => "12.000", 
+                "gambar" => "sablon.jpg",
+                "kategori" => "Kaos"
+            ],
+            [
+                "nama" => "Undangan", 
+                "harga" => "12.000", 
+                "gambar" => "undangan.jpg",
+                "kategori" => "Event"
+            ],
+        ];
+    }
+}
